148c6a2d97f8e875e2b94584d2712fe6a7fb7277
Pristio <rieu.alexandre.sio@gmail.com>
2021-12-17 23:27:22 +0100

    historique

--- clock_gift.js
+++ clock_gift.js
@@ -1,77 +1,3 @@
-class Swipe {
-    constructor(element) {
-        this.xDown = null;
-        this.yDown = null;
-        this.element = typeof(element) === 'string' ? document.querySelector(element) : element;
-
-        this.element.addEventListener('touchstart', function(evt) {
-            this.xDown = evt.touches[0].clientX;
-            this.yDown = evt.touches[0].clientY;
-        }.bind(this), false);
-
-    }
-
-    onLeft(callback) {
-        this.onLeft = callback;
-
-        return this;
-    }
-
-    onRight(callback) {
-        this.onRight = callback;
-
-        return this;
-    }
-
-    onUp(callback) {
-        this.onUp = callback;
-
-        return this;
-    }
-
-    onDown(callback) {
-        this.onDown = callback;
-
-        return this;
-    }
-
-    handleTouchMove(evt) {
-        if ( ! this.xDown || ! this.yDown ) {
-            return;
-        }
-
-        var xUp = evt.touches[0].clientX;
-        var yUp = evt.touches[0].clientY;
-
-        this.xDiff = this.xDown - xUp;
-        this.yDiff = this.yDown - yUp;
-
-        if ( Math.abs( this.xDiff ) > Math.abs( this.yDiff ) ) { // Most significant.
-            if ( this.xDiff > 0 ) {
-                this.onLeft();
-            } else {
-                this.onRight();
-            }
-        } else {
-            if ( this.yDiff > 0 ) {
-                this.onUp();
-            } else {
-                this.onDown();
-            }
-        }
-
-        // Reset values.
-        this.xDown = null;
-        this.yDown = null;
-    }
-
-    run() {
-        this.element.addEventListener('touchmove', function(evt) {
-            this.handleTouchMove(evt).bind(this);
-        }.bind(this), false);
-    }
-}
-
 let links_indices = [
     "2021_12_16-gh6n483y2.html",
     "2021_12_17_a1l5b3o13.html",
@@ -82,16 +8,28 @@ const indexUrl = links_indices.indexOf(document.location.toString().split("/").s
 const indexAvant = indexUrl-1;
 const indexApres = indexUrl+1;
 
-// Get the element yourself.
-var swiper = new Swipe(document);
-swiper.onLeft(function() {
-    if(typeof links_indices[indexApres] !== "undefined") {
-        document.location.href = "../indice" + (indexApres + 1) + "/" + links_indices[indexApres];
-    }
-});
-swiper.onRight(function() {
+let left_hist = document.createElement("div");
+left_hist.style.position = "fixed";
+left_hist.style.width = "50vw";
+left_hist.style.height = "100vh";
+left_hist.style.top = "0";
+left_hist.style.left = "0";
+left_hist.addEventListener("click", function (){
     if(typeof links_indices[indexAvant] !== "undefined") {
         document.location.href = "../indice" + (indexAvant + 1) + "/" + links_indices[indexAvant];
     }
-});
-swiper.run();
+})
+document.body.appendChild(left_hist);
+
+let right_hist = document.createElement("div");
+right_hist.style.position = "fixed";
+right_hist.style.width = "50vw";
+right_hist.style.height = "100vh";
+right_hist.style.top = "0";
+right_hist.style.right = "0";
+right_hist.addEventListener("click", function (){
+    if(typeof links_indices[indexApres] !== "undefined") {
+        document.location.href = "../indice" + (indexApres + 1) + "/" + links_indices[indexApres];
+    }
+})
+document.body.appendChild(right_hist);
