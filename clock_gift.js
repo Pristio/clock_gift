5ede6a8a2078254c705eafe2dfc1b4b1b11291a1
Pristio <rieu.alexandre.sio@gmail.com>
2021-12-17 23:06:26 +0100

    historique

--- clock_gift.js
+++ clock_gift.js
@@ -1,3 +1,77 @@
+class Swipe {
+    constructor(element) {
+        this.xDown = null;
+        this.yDown = null;
+        this.element = typeof(element) === 'string' ? document.querySelector(element) : element;
+
+        this.element.addEventListener('touchstart', function(evt) {
+            this.xDown = evt.touches[0].clientX;
+            this.yDown = evt.touches[0].clientY;
+        }.bind(this), false);
+
+    }
+
+    onLeft(callback) {
+        this.onLeft = callback;
+
+        return this;
+    }
+
+    onRight(callback) {
+        this.onRight = callback;
+
+        return this;
+    }
+
+    onUp(callback) {
+        this.onUp = callback;
+
+        return this;
+    }
+
+    onDown(callback) {
+        this.onDown = callback;
+
+        return this;
+    }
+
+    handleTouchMove(evt) {
+        if ( ! this.xDown || ! this.yDown ) {
+            return;
+        }
+
+        var xUp = evt.touches[0].clientX;
+        var yUp = evt.touches[0].clientY;
+
+        this.xDiff = this.xDown - xUp;
+        this.yDiff = this.yDown - yUp;
+
+        if ( Math.abs( this.xDiff ) > Math.abs( this.yDiff ) ) { // Most significant.
+            if ( this.xDiff > 0 ) {
+                this.onLeft();
+            } else {
+                this.onRight();
+            }
+        } else {
+            if ( this.yDiff > 0 ) {
+                this.onUp();
+            } else {
+                this.onDown();
+            }
+        }
+
+        // Reset values.
+        this.xDown = null;
+        this.yDown = null;
+    }
+
+    run() {
+        this.element.addEventListener('touchmove', function(evt) {
+            this.handleTouchMove(evt).bind(this);
+        }.bind(this), false);
+    }
+}
+
 let links_indices = [
     "2021_12_16-gh6n483y2.html",
     "2021_12_17_a1l5b3o13.html",
@@ -8,45 +82,16 @@ const indexUrl = links_indices.indexOf(document.location.toString().split("/").s
 const indexAvant = indexUrl-1;
 const indexApres = indexUrl+1;
 
-let xDown = null;
-let yDown = null;
-document.addEventListener('touchstart', handleTouchStart, false);
-document.addEventListener('touchmove', handleTouchMove, false);
-
-function getTouches(evt) {
-    return evt.touches ||             // browser API
-        evt.originalEvent.touches; // jQuery
-}
-
-function handleTouchStart(evt) {
-    const firstTouch = getTouches(evt)[0];
-    xDown = firstTouch.clientX;
-    yDown = firstTouch.clientY;
-}
-
-function handleTouchMove(evt) {
-    if (!xDown || !yDown) {
-        return;
+// Get the element yourself.
+var swiper = new Swipe(document);
+swiper.onLeft(function() {
+    if(typeof links_indices[indexApres] !== "undefined") {
+        document.location.href = "../indice" + (indexApres + 1) + "/" + links_indices[indexApres];
     }
-
-    let xUp = evt.touches[0].clientX;
-    let yUp = evt.touches[0].clientY;
-
-    let xDiff = xDown - xUp;
-    let yDiff = yDown - yUp;
-
-    if(Math.abs(xDiff) > Math.abs(yDiff)) {/*most significant*/
-        if(xDiff > 0) {
-            if(typeof links_indices[indexApres] !== "undefined") {
-                document.location.href = "../indice" + (indexApres + 1) + "/" + links_indices[indexApres];
-            }
-        }else{
-            if(typeof links_indices[indexAvant] !== "undefined") {
-                document.location.href = "../indice" + (indexAvant + 1) + "/" + links_indices[indexAvant]
-            }
-        }
+});
+swiper.onRight(function() {
+    if(typeof links_indices[indexAvant] !== "undefined") {
+        document.location.href = "../indice" + (indexAvant + 1) + "/" + links_indices[indexAvant];
     }
-
-    xDown = null;
-    yDown = null;
-}
+});
+swiper.run();
